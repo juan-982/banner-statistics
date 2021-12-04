79d466abb4c839ad90d0ff48fecef8c2707578cf
juan-982 <55919260+juan-982@users.noreply.github.com>
2021-12-05 02:05:38 +0800

    Update query parameters

--- integrations/miHoYo/client.py
+++ integrations/miHoYo/client.py
@@ -1,19 +1,16 @@
-import os, re
+import os, re, time
 import utils.http_client as http_client
 
-from urllib.parse import urlparse, parse_qsl, urlencode, unquote
+from datetime import datetime
+from models.wish import Wish
+from urllib.parse import urlparse, parse_qsl, urlencode
 
 _LOG_PATH = os.environ["USERPROFILE"] + "/AppData/LocalLow/miHoYo/Genshin Impact/output_log.txt"
 _REGEX = "^OnGetWebViewPageFinish:https:\/\/webstatic-sea\.mihoyo\.com\/hk4e\/event\/.*\/log$"
+_LAST_VISIT = None
 
-_GACHA_TYPES = {
-    "beginner": "100",
-    "standard": "200",
-    "character": "301",
-    "weapon": "302",
-    }
-
-def index(gacha_type, page, end_id=0, size=20):
+def index(gacha_type, page=1, end_id="0", size=20):
+    global _LAST_VISIT
     log = None
 
     with open(_LOG_PATH, "r") as log_file:
@@ -22,9 +19,28 @@ def index(gacha_type, page, end_id=0, size=20):
             if m: # If we have a match...
                 log = line
                
+    if log is None:
+        raise Exception("Unable to find matching log") 
+    
     url = urlparse(log.replace("OnGetWebViewPageFinish:", "", 1))
-    url = url._replace(netloc="hk4e-api-os.mihoyo.com", path="event/gacha_info/api/getGachaLog")
 
+    params = dict(parse_qsl(url.query))
+    params.update(
+        {
+            "lang": "en-us", 
+            "size": size, 
+            "gacha_type": Wish.GACHA_TYPES[gacha_type], 
+            "page": page, 
+            "end_id": end_id
+            }
+        )
+
+    url = url._replace(netloc="hk4e-api-os.mihoyo.com", path="event/gacha_info/api/getGachaLog", query=urlencode(params), fragment="")
+
+    if _LAST_VISIT is not None and abs((datetime.now() - _LAST_VISIT).total_seconds()) < 3:
+        time.sleep(3)
+
+    _LAST_VISIT = datetime.now()
     response = http_client.get(url.geturl())
 
     return response.json()
