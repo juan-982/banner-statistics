a0e8c49370783995abf08b27099ae50085b3a88d
juan-982 <55919260+juan-982@users.noreply.github.com>
2021-12-05 02:04:21 +0800

    Refactor service

--- services/wish_service.py
+++ services/wish_service.py
@@ -4,35 +4,52 @@ import integrations.miHoYo.client as client
 
 from models.wish import Wish
 
-def retrieve_history():
-    result = client.index("character", 1)
-    for element in result["data"]["list"]:
-        print(element["id"])
-        print(element["time"])
-        print(element["gacha_type"])
-        print(element["name"])
-        print(element["item_type"])
-        print(element["rank_type"])
-	
+def retrieve_history(gacha_type, load_all=False):
+    end_id = "0"
+    result = client.index(gacha_type, end_id=end_id)
 
-def create(external_id, time, name, gacha_type, item_type, rank_type):
-    cursor = sqlite3.connect(db_manager.DB_PATH).cursor()
-    cursor.execute("""
-      INSERT INTO wishes (external_id, time, name, gacha_type, item_type, rank_type) VALUES(?, ?, ?, ?, ?)
-    """, (external_id, time, name, gacha_type, item_type, rank_type))
+    while len(result["data"]["list"]) > 0:
+        count = create(result["data"]["list"])
+
+        if not load_all and count < len(result["data"]["list"]):
+            break
+
+        end_id = result["data"]["list"][-1]["id"]
+        result = client.index(gacha_type, end_id=end_id)
+
+def create(list):
+    count = 0
+    with sqlite3.connect(db_manager.DB_PATH) as conn:
+        cursor = conn.cursor()
+        for element in list:
+            cursor.execute("""
+                SELECT external_id, gacha_type FROM wishes WHERE external_id = ? AND gacha_type = ?
+            """, (element["id"], element["gacha_type"]))
+            record = cursor.fetchone()
+
+            if record is None:
+                count += 1
+                cursor.execute("""
+                    INSERT INTO wishes (external_id, time, name, gacha_type, item_type, rank_type) VALUES(?, ?, ?, ?, ?, ?)
+                """, (element["id"], element["time"], element["name"], element["gacha_type"], element["item_type"], element["rank_type"]))
+                
+        cursor.close()
+        return count
 
 def list(gacha_type):
-    cursor = sqlite3.connect(db_manager.DB_PATH).cursor()
-    cursor.execute("""
-      SELECT id, external_id, time, name, gacha_type, item_type, rank_type
-        FROM wishes
-        WHERE gacha_type = ?
-    """, gacha_type)
-    records = cursor.fetchall()
+    with sqlite3.connect(db_manager.DB_PATH) as conn:
+        cursor = conn.cursor()
+        cursor.execute("""
+          SELECT id, external_id, time, name, gacha_type, item_type, rank_type
+            FROM wishes
+            WHERE gacha_type = ?
+        """, (Wish.GACHA_TYPES[gacha_type],))
+        records = cursor.fetchall()
 
-    wishes = []
+        wishes = []
 
-    for record in records:
-      wishes.append(Wish(record[0], record[1], record[2], record[3], record[4], record[5], record[6]))
+        for record in records:
+          wishes.append(Wish(record[0], record[1], record[2], record[3], record[4], record[5], record[6]))
 
-    return wishes
+        cursor.close()
+        return wishes
